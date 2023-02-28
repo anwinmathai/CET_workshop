d96e3d0c537e9b97be0a12d6ff4a601ca94b0fc0
anwinmathai <anwinmathai7@gmail.com>
2023-02-28 19:11:33 +0530

    homework

---
+++ Homework.py
@@ -0,0 +1,6 @@
+# Develop a basic calculator hint: use if .. elif.. else
+
+# 1. Read two numbers from user
+#2. Read an operation to do : Example "+" or "-"
+#3. Check the operation and perform the corresponding operation
+#4. Print the result

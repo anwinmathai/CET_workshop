4fac83aeb88d064dec32e065e6841bf0757c5c98
anwinmathai <anwinmathai7@gmail.com>
2023-02-28 18:34:49 +0530

    Can you vote

---
+++ Vote.py
@@ -0,0 +1,17 @@
+# Program to find if the age can be found
+#with a hard coded age
+age= 9
+if age>18:
+    print("You can vote Bro")
+    print("Have fun...")
+else:
+    print(18-age,"years later")
+
+
+# Taking input from the user
+age= int(input(" Enter your age please:"))
+if age>18:
+    print("You can vote Bro")
+    print("Have fun...")
+else:
+    print(18-age,"years later")

dbe634bf3821b21a687c1d3e299f93b8b030a5a4
anwinmathai <anwinmathai7@gmail.com>
2023-03-02 18:16:32 +0530

    password program

--- Username and password using dictionary.py
+++ Username and password using dictionary.py
@@ -1,13 +1,13 @@
 '''
 We have a database called users to save the username and password as dictionary
 '''
-users={"Anwin":"nothin","Arvind":"hi","Athira":"054","Pearly": "8sfsd"}
+users={"Anwin":"nothin","Arvind":"hi","Athira":"054","Pearly": "8sfsd"} #Database
 
-username=input("Enter the username :")
-if username in users:
+username=input("Enter the username :") #Getting the username from the user
+if username in users:             #Checking the password of the username by getting the password as well. 
     password=input("Enter the password: ")
-    correctpassword=users[username]
+    correctpassword=users[username]        #Correctpassword is present in the Database ie. dictionary
     if password==correctpassword:
         print("It's right u can log in")
     else:
-        print("SOrry")
+        print("SOrry,the password is wrong")

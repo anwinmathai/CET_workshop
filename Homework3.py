66f769c200bd32fa4a0e679a491d8d686b836ecb
anwinmathai <anwinmathai7@gmail.com>
2023-03-09 17:18:13 +0530

    homework3

--- Homework3.py
+++ Homework3.py
@@ -13,11 +13,30 @@ Also verify if they have already added and if they havent, allow sign in
 Signup as well
 
 '''
-users={} #Database
 
-for n in range(0,3):
+'''' Sign up'''
+users={} #Database blank at first
+print("...............Sign up...............")
+j=int(input("Enter the number of users being taken in:"))
+for n in range(0,j):
     username=input("Enter the username :") #Getting the username from the user
-    if username!=users.keys():
+    if username not in users:
             users[username]=input("Enter the password for username :")
     else:
             print("Enter a valid username.")
+
+
+
+
+'''           SiGN in                                                        '''
+''' Sign in'''
+
+print("...............Sign in.........................")
+username=input("Enter the username:")
+if username in users:             #Checking the password of the username by getting the password as well. 
+    password=input("Enter the password: ")
+    correctpassword=users[username]        #Correctpassword is present in the Database ie. dictionary
+    if password==correctpassword:
+        print("It's right. You are logged in.")
+    else:
+        print("Sorry,the password is wrong.")

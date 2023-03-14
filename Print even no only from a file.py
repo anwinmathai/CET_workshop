545856755b10909a367fc9034fd851e58f25662c
anwinmathai <anwinmathai7@gmail.com>
2023-03-14 18:20:23 +0530

    filehandling2

--- Print even no only from a file.py
+++ Print even no only from a file.py
@@ -6,7 +6,7 @@ file=open("random.txt","r") # for reading numbers.txt in read mode and text is r
 print(type(file)) #to know what type of data is read and saved in file. its <class '_io.TextIOWrapper'>
 
 
-
+file.close()
 """
 This is a wrong method as a two digit at final line will be considered as only one digit
 
@@ -17,9 +17,10 @@ for content in contents:
             print(content)
 
 """
+#1
 
 #.strip() removes spaces or \n characters in the string
-
+file=open("random.txt","r") 
 contents=file.readlines() #Makes a list of the characters
 for element in contents:   #loop through the content text
     if int(element.strip())%2==0:  #strips off \n and white spaces and converts the text to integer
@@ -29,3 +30,16 @@ for element in contents:   #loop through the content text
 
 
 file.close() # Close each time it is open
+
+
+#2
+#Comma separated numbers in the file random2.txt
+file=open("random2.txt","r") 
+contents=file.read() #read directly then only use split. We can only split the line. not list.
+contents=contents.split(",") # As per which character the split is to be done.
+print(contents) #To print the content which was split using the split function
+
+for element in contents:   #loop through the content text
+    if int(element.strip())%2==0:  #strips off \n and white spaces and converts the text to integer
+        print(element.strip())
+file.close()

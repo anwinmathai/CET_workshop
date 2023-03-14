86f9728497659c93bdc78d60d134e2aa58d5f754
anwinmathai <anwinmathai7@gmail.com>
2023-03-14 18:40:08 +0530

    filehandling2

--- Print even no only from a file.py
+++ Print even no only from a file.py
@@ -18,7 +18,6 @@ for content in contents:
 
 """
 #1
-
 #.strip() removes spaces or \n characters in the string
 file=open("random.txt","r") 
 contents=file.readlines() #Makes a list of the characters
@@ -43,3 +42,47 @@ for element in contents:   #loop through the content text
     if int(element.strip())%2==0:  #strips off \n and white spaces and converts the text to integer
         print(element.strip())
 file.close()
+
+
+#3. "w" mode ie. write mode
+# To write something to a file if its even
+# By default python makes a file, else it will delete the file already present.
+file_to_read = open("numbers.txt")
+file_to_write= open("evens.txt","w")
+for num in contents:
+    if int(num) %2==0:
+        #Write the number to a different file
+        file_to_write.write(num)
+        file_to_write.write("\n") # To write in the next line
+        print(num)
+file.close()
+
+
+#4. "a" mode ie. append mode
+
+file_to_read = open('numbers.txt')
+file_to_write= open("evens.txt","a")
+for num in contents:
+    if int(num) %2==0:
+        #Write the number to a different file
+        file_to_write.write(num)
+        file_to_write.write("\n") # To write in the next line
+        print(num)
+file.close()
+
+
+
+
+#5. with open()
+# Automatically closes the file 
+with open("evens.txt","r") as file:
+    print(file.read())
+
+#6 with open to do write
+# Automatically closes the file
+with open("evens.txt","a") as file:
+    print(file.write("End"))
+
+with open("evens.txt","r") as file:
+    print(file.read())
+    
